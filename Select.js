7fb19bec4317709cf390b2c1eb5601b8815c7565
tugbargn <40305902+tugbargn@users.noreply.github.com>
2022-04-14 15:05:23 +0300

    Create Select.js

---
+++ Select.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+function MyForm() {
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute"
+  );
+
+  const handleChange = (event) => {
+    setTextarea(event.target.value)
+  }
+
+  return (
+    <form>
+      <textarea value={textarea} onChange={handleChange} />
+    </form>
+  )
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
